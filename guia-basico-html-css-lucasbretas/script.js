d6c37216888e621e2bd777710e41ef93d32b13f8
Lucas Bretas <66535036+lucasfront@users.noreply.github.com>
2021-03-05 10:06:59 -0300

    Add files via upload

---
+++ guia-basico-html-css-lucasbretas/script.js
@@ -0,0 +1,3 @@
+// este arquivo foi criado apenas para mostrar como deve ser estruturado um arquivo html, separando os demais arquivos css e js e linkando os mesmos através de links.
+
+// sinta-se avontade para fazer modificações nesse arquivo e aprender ainda mais sobre.
